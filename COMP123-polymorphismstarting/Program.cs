418b4ebd595a02f7788db3f5bac0cdecd443350d
Ravleen Kaur <ravleenkaurtumber@gmail.com>
2020-07-12 00:22:51 -0400

    Add project files.

---
+++ COMP123-polymorphismstarting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP123_polymorphismstarting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
