76e38eb1761396c5e556fb0d7814298d1aab6afb
Ravleen Kaur <ravleenkaurtumber@gmail.com>
2020-07-12 00:56:35 -0400

    added a gameobject

---
+++ COMP123-polymorphismstarting/GameObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP123_polymorphismstarting
+{
+    class GameObject
+    {
+        // PRIVTE INSTANCE MEMBERS
+        private  string m_name;
+        //PUBLIC PROPERTIES
+        public Transform Transform { get; set; }
+        public string Name
+        {
+            get
+            {
+                return this.m_name;
+            }
+            set
+            {
+                this.m_name = value;
+            }
+        }
+        //CONSTRUCTOR(s)
+        public GameObject(string name)
+        {
+            this.Name = name;
+        }
+        //PRIVATE METHODS
+        //PUBLIC METHODS
+
+    }
+}
