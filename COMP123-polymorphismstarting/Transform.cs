b1c97406e8cdc81da4eea50371ae937f1f2e245c
Ravleen Kaur <ravleenkaurtumber@gmail.com>
2020-07-12 00:47:44 -0400

    add transform struct

---
+++ COMP123-polymorphismstarting/Transform.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace COMP123_polymorphismstarting
+{
+    struct Transform
+    {
+        public Vector2D position;
+        public Vector2D rotation;
+        public Vector2D scale;
+    }
+}
