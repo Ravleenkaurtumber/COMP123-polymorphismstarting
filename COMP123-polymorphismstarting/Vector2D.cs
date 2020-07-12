31e92f32704e413af19900af7b8e33cd0bcce98c
Ravleen Kaur <ravleenkaurtumber@gmail.com>
2020-07-12 00:37:48 -0400

    added vector 2d struct

---
+++ COMP123-polymorphismstarting/Vector2D.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP123_polymorphismstarting
+{
+    struct  Vector2D
+    {
+        public float x;
+        public float y;
+    }
+}
